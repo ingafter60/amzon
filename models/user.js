845783e89215cd005b3d8cfa79cb974c517ba1cc
ing <inyoman_gurnitha@yahoo.com>
2019-11-21 09:18:22 +0700

    Sec1-5. Connect to the database (MongoDB)

--- models/user.js
+++ models/user.js
@@ -9,3 +9,6 @@ const UserSchema = new Schema({
 });
 
 module.exports = mongoose.model('User', 'UserSchema');
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+// App as express object
 const app = express();
 
+// DB - Connect to mongodb
+mongoose.connect(
+  'mongodb+srv://ing:wk7rI1rD0HNKcbZl@amazon-clone-oo0xb.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Connected to databae ..');
+    }
+  }
+);
 // Middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
@@ -16,8 +30,8 @@ app.get('/', (req, res) => {
 
 // POST - send data from frontend to backend
 app.post('/', (req, res) => {
-  console.log(req.body.name)
-})
+  console.log(req.body.name);
+});
 
 app.listen(3000, err => {
   if (err) {
